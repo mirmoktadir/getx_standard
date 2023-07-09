3b8fc45beef74575059d77c39d97e87cddc77c2a
Moktadir <moktadir@Moktadirs-iMac-Pro.local>
2023-07-10 05:48:06 +0600

    update navbar and orientation

--- lib/app/modules/home-with-graphql/views/graphql_view.dart
+++ lib/app/modules/home-with-graphql/views/graphql_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:getx_standard/app/components/navbar/navbar_controller.dart';
 import 'package:iconly/iconly.dart';
 
 import '../../../../config/theme/my_fonts.dart';
@@ -14,13 +13,13 @@ class GraphQLView extends GetView<GraphQLController> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    final navController = Get.put(NavbarController());
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('GraphQL'),
         actions: [
           IconButton(
-              onPressed: () => navController.selectedIndex.value = 0,
+              onPressed: () => controller.navController.selectedIndex.value = 0,
               icon: const Icon(
                 IconlyBold.home,
                 color: Colors.white,
@@ -28,86 +27,89 @@ class GraphQLView extends GetView<GraphQLController> {
         ],
         centerTitle: true,
       ),
-      body: Obx(() => controller.isError.value == true
+      body: Obx(() =>
+      controller.isError.value == true
           ? EmptyWidget(onPressed: () async => await controller.getAlbums())
           : RefreshIndicator(
-              color: theme.primaryColor,
-              onRefresh: () async => await controller.getAlbums(),
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: RawScrollbar(
-                  thumbColor: theme.primaryColor,
-                  radius: const Radius.circular(100),
-                  thickness: 5,
-                  interactive: true,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: 150.w,
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller.updateTodo();
-                            },
-                            child: const Text("Run mutation")),
-                      ),
-                      SizedBox(height: 10.h),
-                      controller.mutationResult.isEmpty
-                          ? const SizedBox()
-                          : Padding(
-                              padding: EdgeInsets.symmetric(vertical: 20.h),
-                              child: Text(
-                                  "RESULT :${controller.mutationResult.value}"),
-                            ),
-                      Expanded(
-                        child: ListView.separated(
-                          itemCount: controller.userList.length,
-                          physics: const BouncingScrollPhysics(),
-                          padding: EdgeInsets.zero,
-                          separatorBuilder: (_, __) => SizedBox(
-                            height: 20.h,
-                          ),
-                          itemBuilder: (ctx, index) => Container(
-                            padding: const EdgeInsets.all(5),
-                            width: double.infinity,
-                            color: theme.canvasColor,
-                            child: Center(
-                              child: Container(
-                                width: double.infinity,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(10.r)),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color:
-                                            theme.primaryColor.withOpacity(.3),
-                                        blurRadius: 7.0,
-                                        spreadRadius: 4,
-                                        offset: const Offset(0, 3),
-                                      )
-                                    ]),
-                                child: Center(
-                                  child: Text(
-                                    controller.userList[index]["name"] ?? "",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: MyFonts.headline6TextSize,
-                                      fontWeight: FontWeight.w500,
-                                      color: theme.primaryColor,
-                                    ),
+        color: theme.primaryColor,
+        onRefresh: () async => await controller.getAlbums(),
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: RawScrollbar(
+            thumbColor: theme.primaryColor,
+            radius: const Radius.circular(100),
+            thickness: 5,
+            interactive: true,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 150.w,
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        await controller.updateTodo();
+                      },
+                      child: const Text("Run mutation")),
+                ),
+                SizedBox(height: 10.h),
+                controller.mutationResult.isEmpty
+                    ? const SizedBox()
+                    : Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.h),
+                  child: Text(
+                      "RESULT :${controller.mutationResult.value}"),
+                ),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: controller.userList.length,
+                    physics: const BouncingScrollPhysics(),
+                    padding: EdgeInsets.zero,
+                    separatorBuilder: (_, __) =>
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                    itemBuilder: (ctx, index) =>
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          width: double.infinity,
+                          color: theme.canvasColor,
+                          child: Center(
+                            child: Container(
+                              width: double.infinity,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(10.r)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color:
+                                      theme.primaryColor.withOpacity(.3),
+                                      blurRadius: 7.0,
+                                      spreadRadius: 4,
+                                      offset: const Offset(0, 3),
+                                    )
+                                  ]),
+                              child: Center(
+                                child: Text(
+                                  controller.userList[index]["name"] ?? "",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: MyFonts.headline6TextSize,
+                                    fontWeight: FontWeight.w500,
+                                    color: theme.primaryColor,
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
                   ),
                 ),
-              ),
-            )),
+              ],
+            ),
+          ),
+        ),
+      )),
     );
   }
 }
