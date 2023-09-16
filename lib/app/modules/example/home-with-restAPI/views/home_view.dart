7cf644f2fbda44d6224bda20e4677fb3aa30712a
Moktadir <moktadir@Moktadirs-iMac-Pro.local>
2023-09-16 06:00:48 +0600

    Tween Animation

--- lib/app/modules/example/home-with-restAPI/views/home_view.dart
+++ lib/app/modules/example/home-with-restAPI/views/home_view.dart
@@ -34,46 +34,71 @@ class HomeView extends GetView<HomeController> {
           : RefreshIndicator(
               color: theme.primaryColor,
               onRefresh: () async => await controller.getPostList(),
-              child: Padding(
-                padding: EdgeInsets.all(18.r),
-                child: RawScrollbar(
-                  thumbColor: theme.primaryColor,
-                  radius: Radius.circular(100.r),
-                  thickness: 5,
-                  interactive: true,
-                  child: ListView.separated(
-                    itemCount: controller.postList.length,
-                    physics: const BouncingScrollPhysics(),
-                    padding: EdgeInsets.zero,
-                    separatorBuilder: (_, __) => SizedBox(
-                      height: 20.h,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: 1),
+                    duration: const Duration(milliseconds: 1000),
+                    builder:
+                        (BuildContext context, double value, Widget? child) {
+                      return Opacity(
+                        opacity: value,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: value * 18.r),
+                          child: child,
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "GetX Standard",
+                      style: theme.textTheme.displaySmall,
                     ),
-                    itemBuilder: (ctx, index) => SplashContainer(
-                      radius: 15,
-                      onPressed: () {
-                        controller.title.value =
-                            controller.postList[index].title ?? "";
-                        controller.body.value =
-                            controller.postList[index].body ?? "";
-                        Get.toNamed(Routes.POST_DETAIL);
-                      },
+                  ),
+                  Expanded(
+                    child: RawScrollbar(
+                      thumbColor: theme.primaryColor,
+                      radius: Radius.circular(100.r),
+                      thickness: 5,
+                      interactive: true,
                       child: Padding(
-                        padding: EdgeInsets.all(8.r),
-                        child: Center(
-                          child: Text(
-                            controller.postList[index].title ?? "",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: MyFonts.headline6TextSize,
-                              fontWeight: FontWeight.w500,
-                              color: theme.primaryColor,
+                        padding: EdgeInsets.all(18.r),
+                        child: ListView.separated(
+                          itemCount: controller.postList.length,
+                          physics: const BouncingScrollPhysics(),
+                          padding: EdgeInsets.zero,
+                          separatorBuilder: (_, __) => SizedBox(
+                            height: 20.h,
+                          ),
+                          itemBuilder: (ctx, index) => SplashContainer(
+                            radius: 15,
+                            onPressed: () {
+                              controller.title.value =
+                                  controller.postList[index].title ?? "";
+                              controller.body.value =
+                                  controller.postList[index].body ?? "";
+                              Get.toNamed(Routes.POST_DETAIL);
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(8.r),
+                              child: Center(
+                                child: Text(
+                                  controller.postList[index].title ?? "",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: MyFonts.headline6TextSize,
+                                    fontWeight: FontWeight.w500,
+                                    color: theme.primaryColor,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
             )),
     );
