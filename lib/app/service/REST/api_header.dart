4a5323cf650cddc6a171993b27bbdd103632a596
Mir Moktadir <38207101+mirmoktadir@users.noreply.github.com>
2025-05-03 15:30:08 +0600

    Update api_header.dart

--- lib/app/service/REST/api_header.dart
+++ lib/app/service/REST/api_header.dart
@@ -1,33 +1,41 @@
+import '../../data/local/my_shared_pref.dart';
+
 class Header {
   // SECURE HEADER
-  static Map<String, dynamic> secureHeader = {
-    "Authorization": "Bearer 'token'",
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  };
+  static Future<Map<String, String>> getSecureHeader() async {
+    String? token = await MySharedPref.getAuthToken();
+
+    return {
+      "Authorization": "Bearer $token",
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+  }
 
   // DEFAULT HEADER
-  static Map<String, dynamic> defaultHeader = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  };
+  static Map<String, String> get defaultHeader => {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      };
 
   // DEFAULT MULTIPART-HEADER
-  static Map<String, dynamic> defaultMultipartHeader = {
-    'Accept': 'application/json',
-    'Content-Type': 'multipart/form-data',
-  };
+  static Map<String, String> get defaultMultipartHeader => {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data',
+      };
 
   // SECURE MULTIPART HEADER
-  static Map<String, dynamic> secureMultipartHeader = {
-    "Authorization": "Bearer 'token'",
-    'Accept': 'application/json',
-    'Content-Type': 'multipart/form-data',
-  };
+  static Future<Map<String, String>> getSecureMultipartHeader() async {
+    String? token = await MySharedPref.getAuthToken();
+    return {
+      "Authorization": "Bearer $token",
+      'Accept': 'application/json',
+      'Content-Type': 'multipart/form-data',
+    };
+  }
 
-  /// ***************** PERSONAL CUSTOM API HEADER ***************** ///
   // RAPID API HEADER
-  static Map<String, dynamic> rapidApiHeader = {
+  static const Map<String, String> rapidApiHeader = {
     "X-RapidAPI-Key": "05741bc39bmsh35c797d07e59651p13baa5jsn936cd89ffe07",
   };
 }
